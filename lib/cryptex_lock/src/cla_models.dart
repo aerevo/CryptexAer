4f3fce8c46dd9c9150ec17554affdeb0c8476974
aerevo <aerevo554@gmail.com>
2025-12-25 23:58:31 +0800

    Update cla_models.dart

--- lib/cryptex_lock/src/cla_models.dart
+++ lib/cryptex_lock/src/cla_models.dart
@@ -2,12 +2,12 @@ enum ClaResult { success, fail, jammed }
 
 class ClaConfig {
   final Duration minSolveTime;
-  final double minShake;
-  final int thresholdAmount;
+  final double minShake; // sederhana ~0.18
+  final List<int> secret; // kombinasi sebenar
 
   const ClaConfig({
     required this.minSolveTime,
     required this.minShake,
-    required this.thresholdAmount,
+    required this.secret,
   });
 }
