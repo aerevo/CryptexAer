7eecf4665f56bd1e7e7c64d878e419d41fcb44e5
aerevo <aerevo554@gmail.com>
2025-12-26 11:53:22 +0800

    Update cla_config.dart

--- lib/cryptex_lock/src/cla_config.dart
+++ lib/cryptex_lock/src/cla_config.dart
@@ -1,8 +1,19 @@
+// lib/cryptex_lock/src/cla_config.dart
+
 class ClaConfig {
+  /// Kombinasi rahsia untuk kunci (Senarai integer)
   final List<int> secret;
+  
+  /// Masa minimum interaksi manusia (Anti-Bot Timer)
   final Duration minSolveTime;
+  
+  /// Tahap gegaran minimum untuk membuktikan kehadiran fizikal
   final double minShake;
+  
+  /// Tempoh masa sistem terkunci jika 'Zero Trap' disentuh atau bot dikesan
   final Duration jamCooldown;
+  
+  /// Nilai transaksi minimum untuk mengaktifkan CLA
   final double thresholdAmount;
 
   const ClaConfig({
