08019d486350fb052f8748ec266e71ece64c07ea
aerevo <aerevo554@gmail.com>
2025-12-26 09:10:24 +0800

    Update cryptex_lock.dart

--- lib/cryptex_lock/cryptex_lock.dart
+++ lib/cryptex_lock/cryptex_lock.dart
@@ -1,5 +1,5 @@
 library cryptex_lock;
 
-export 'src/cla_widget.dart';
+export 'src/cla_config.dart';
 export 'src/cla_controller.dart';
-export 'src/cla_models.dart';
+export 'src/cla_widget.dart';
