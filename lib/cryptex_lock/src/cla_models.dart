4d2a3202eba23230a6c625e6d13595b50d5ed933
aerevo <aerevo554@gmail.com>
2026-01-18 19:28:53 +0800

    Update cla_models.dart

--- lib/cryptex_lock/src/cla_models.dart
+++ lib/cryptex_lock/src/cla_models.dart
@@ -1,6 +1,7 @@
 // lib/cryptex_lock/src/cla_models.dart
 
 import 'security_core.dart';
+import 'motion_models.dart'; // Import models from here
 
 enum SecurityState {
   LOCKED,
@@ -8,36 +9,6 @@ enum SecurityState {
   UNLOCKED,
   SOFT_LOCK,
   HARD_LOCK,
-}
-
-class MotionEvent {
-  final double magnitude;
-  final DateTime timestamp;
-  final double deltaX;
-  final double deltaY;
-  final double deltaZ;
-
-  MotionEvent({
-    required this.magnitude,
-    required this.timestamp,
-    this.deltaX = 0,
-    this.deltaY = 0,
-    this.deltaZ = 0,
-  });
-}
-
-class TouchEvent {
-  final DateTime timestamp;
-  final double pressure;
-  final double velocityX;
-  final double velocityY;
-
-  TouchEvent({
-    required this.timestamp,
-    required this.pressure,
-    required this.velocityX,
-    required this.velocityY,
-  });
 }
 
 class SecurityEngineConfig {
@@ -58,20 +29,15 @@ class SecurityEngineConfig {
 
 class ClaConfig {
   final List<int> secret;
-
   final Duration minSolveTime;
   final double minShake;
   final double thresholdAmount;
   final int maxAttempts;
-
   final Duration jamCooldown;
   final Duration softLockCooldown;
-
   final bool enableSensors;
-
   final String clientId;
   final String clientSecret;
-
   final SecurityEngineConfig engineConfig;
   final AttestationProvider? attestationProvider;
 
