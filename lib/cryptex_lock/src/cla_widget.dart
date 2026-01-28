80cf4e8d466003e40bae1ff8afb1a69635c69838
aerevo <aerevo554@gmail.com>
2026-01-29 06:43:22 +0800

    Update cla_widget.dart

--- lib/cryptex_lock/src/cla_widget.dart
+++ lib/cryptex_lock/src/cla_widget.dart
@@ -54,7 +54,7 @@ class _CryptexLockState extends State<CryptexLock> {
   static const double verticalOffsetRatio = -0.0648;
   
   // 🔧 IMAGE SCALE (zoom in/out)
-  static const double imageScale = 1.05; // 5% BIGGER
+  static const double imageScale = 1.20; // 20% BIGGER
 
   @override
   void initState() {
